84e9f90cf9ae42d67d85e6e1ae35025c501d4904
Andy Chiang <rdandy@gmail.com>
2023-06-23 16:32:06 +0800

    add 7 segment led all ON

--- ch4/B03_7_seg_led_all_on.py
+++ ch4/B03_7_seg_led_all_on.py
@@ -10,26 +10,9 @@ seg7 = [5, 33, 19, 15, 13, 7, 21, 11]
 # scan =[23, 31, 29, 3]
 for x in seg7:
     GPIO.setup(x, GPIO.OUT)
+
 # for x in scan:
 #     GPIO.setup(x, GPIO.OUT)
 
 for x in range(len(seg7)):
-    GPIO.output(seg7[x], 0)
-
-try:
-    while True:
-        for x in range(len(seg7)):
-            time.sleep(0.3)
-            GPIO.output(seg7[x], 1)
-        time.sleep(0.6)
-        for x in range(len(seg7)):
-            time.sleep(0.3)
-            GPIO.output(seg7[x], 0)
-        time.sleep(1.0)
-
-except KeyboardInterrupt:
-    for x in range(len(seg7)):
-        GPIO.output(seg7[x], 0)
-
-# for x in range(4):
-#     GPIO.output(scan[x], 1)
+    GPIO.output(seg7[x], 1)
