9620d8bd60638484fc6ed809201683404b8ba4e7
Andy Chiang <rdandy@gmail.com>
2023-06-23 12:21:49 +0800

    add buzzer test

--- ch4/B01_buzzer.py
+++ ch4/B01_buzzer.py
@@ -9,28 +9,35 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(PIN_OUT, GPIO.OUT)
 
 while True:
-    ans = int(input("選擇聲音輸出種類<1-3>,按0離開："))
+    ans = int(input("選擇聲音輸出種類<1-4>,按0離開："))
     if ans == 0:
        GPIO.output(PIN_OUT, 0)
        break
 
     if ans == 1:
-       for r in range(1000):
+       for r in range(10000):
            for x in range(250):
                GPIO.output(PIN_OUT, 1)
            for x in range(250):
                GPIO.output(PIN_OUT, 0)
 
     if ans == 2:
-       for r in range(1000):
+       for r in range(10000):
            for x in range(250):
                GPIO.output(PIN_OUT, 1)
            for x in range(1000):
                GPIO.output(PIN_OUT, 0)
 
     if ans == 3:
-       for r in range(1000):
+       for r in range(10000):
            for x in range(1000):
                GPIO.output(PIN_OUT, 1)
            for x in range(250):
+               GPIO.output(PIN_OUT, 0)
+
+    if ans == 4:
+       for r in range(10000):
+           for x in range(500):
+               GPIO.output(PIN_OUT, 1)
+           for x in range(300):
                GPIO.output(PIN_OUT, 0)
